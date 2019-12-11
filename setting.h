c1e463e12ce747c07c30492341552208e57d441e
phil <peychart@mail.pf>
2019-12-11 12:14:12 -1000

    Allows now only one input (powerStrips control)

--- setting.h
+++ setting.h
@@ -1,5 +1,6 @@
 //Ajust the following:
 
+//#define DEBUG
 #define VERSION              "3.0.1"       //Change this value to reset current config on the next boot...
 #define DEFAULTHOSTNAME      "ESP8266"
 //NOTA: no SSID declared (in web interface) will qualify me as a slave candidate...
@@ -7,12 +8,12 @@
 #define WIFISTADELAYRETRY     30000UL
 #define MAXWIFIRETRY          2
 #define WIFIAPDELAYRETRY      300000UL
+#define REFRESH_PERIOD        30
 //#define MEMORYLEAKS           10000UL
 #define SSIDCount()           3
 #define RESTO_VALUES_ON_BOOT  false
 #define REVERSE_OUTPUT        false
 #define DEBOUNCE_TIME         25UL
-#define HOLD_TO_DISABLE_TIMER 5000UL
 
 #define DEFAULT_MQTT_SERVER  "mosquitto.home.lan"
 #ifdef  DEFAULT_MQTT_SERVER
@@ -26,32 +27,33 @@
   #define NOTIFYPORT          8081
 #endif
 
-//#define DEBUG
-
-
 #define LUMIBLOC_RELAY6X_D1MINI
 
 #ifdef LUMIBLOC_SSD_D1MINI
+  #define HOLD_TO_DISABLE_TIMER 3UL       //(s)
   static std::vector<ushort>   _inputPin ={ D5, D6, D7 };             //lumibloc
   static std::vector<ushort>   _outputPin={ D1, D2, D3, D4, D0, D8 };
 #endif
 
 #ifdef LUMIBLOC_RELAY6X_D1MINI
+  #define HOLD_TO_DISABLE_TIMER 3UL       //(s)
   static std::vector<ushort>   _inputPin ={ D5, D6, D7 };             //relay 6x
   static std::vector<ushort>   _outputPin={ D8, D1, D2, D3, D4, D0 };
 //static std::vector<ushort>   _outputPin={ D8, D1, D2, D3, D4, D0, (ushort)-1 }; //with a virtual output...
 #endif
 
 #ifdef SIMPLESWITCH_ESP01
+  #define HOLD_TO_DISABLE_TIMER 3UL       //(s)
   static std::vector<ushort>   _inputPin ={ 2 };             //ESP-01S relay 1x
   static std::vector<ushort>   _outputPin={ 0 };
 #endif
 
 #ifdef POWERSTRIP
+  #define HOLD_TO_DISABLE_TIMER 5UL       //(s)
   static std::vector<ushort>   _inputPin ={ D7 };             //TUYA TYSE3S Power Strip 3x
   static std::vector<ushort>   _outputPin={ D6, D5, D8, D3, D2 };
-#define POWER_LED             D0
-#define BLINKING_POWERLED     1000UL,0UL
-#define WIFISTA_LED           D1
-#define BLINKING_WIFILED      5000UL,250UL
+  #define POWER_LED             D0
+  #define BLINKING_POWERLED     1000UL,0UL
+  #define WIFISTA_LED           D1
+  #define BLINKING_WIFILED      5000UL,250UL
 #endif
